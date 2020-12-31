3e14c9948807eb5baaf128e190d12da45fc6edc0
chrisb3546 <chrisbradycode@gmail.com>
2020-12-31 16:56:59 -0500

    add client list to client index view, add new lift form

--- app/controllers/lifts_controller.rb
+++ app/controllers/lifts_controller.rb
@@ -1,21 +1,38 @@
 class LiftsController < ApplicationController
-    class liftsController < ApplicationController
+    
         def index
+            if params[:client_id] && @client = Client.find_by(id: params[]:id])
+                @lift = @client.lifts.all
+            end
         end
     
         def show
         end
     
         def new
+            if params[:client_id] && @client = Client.find_by(id: params[]:id])
+                @lift = @client.lift.build
+            end
         end
     
         def create
+            if params[:client_id] && @client = Client.find_by(id: params[]:id])
+                @lift = @client.lift.build(lift_params)
+            end
+            if @lift.save
+                redirect_to client_lift_path(@lift.client, @lift)
+            end
         end
     
         def edit
         end
     
         def update
+            if @lift.update(@lift_params)
+                redirect_to client_lift_path(@lift.client, @lift)
+            else
+                render :edit 
+            end
         end
     
         def destroy
@@ -24,8 +41,8 @@ class LiftsController < ApplicationController
         private
     
         def lift_params
-            params.require(:lift).permit(:name, :weight, :ROM, :repetitions, :date, :notes)
+            params.require(:lift).permit(:client_id, :name, :weight, :ROM, :repetitions, :date, :notes)
         end
     
-    end
+
 end
--- app/models/lift.rb
+++ app/models/lift.rb
@@ -1,2 +1,3 @@
 class Lift < ApplicationRecord
+    belongs_to :client
 end
